1bcbb547dda6e2c2293eb1607b4fd76f2ea7dad8
lwronski <woljako@gmail.com>
2017-05-06 12:33:04 +0200

    Add Piece

---
+++ View/Piece.java
@@ -0,0 +1,26 @@
+package App;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+
+import static App.Go.TILE_SIZE;
+
+/**
+ * Created by lukasz on 06.05.2017.
+ */
+public class Piece extends StackPane{
+
+    public Piece(PieceType type, int x, int y) {
+
+        relocate(x*TILE_SIZE, y*TILE_SIZE);
+
+        Ellipse bg = new Ellipse(TILE_SIZE * 0.3125, TILE_SIZE * 0.26);
+        bg.setFill(type == PieceType.BLACK ? Color.BLACK : Color.WHITE);
+
+        bg.setTranslateX((TILE_SIZE - TILE_SIZE * 0.3125 * 2) / 2);
+        bg.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) / 2 + TILE_SIZE * 0.07);
+
+        getChildren().addAll(bg);
+    }
+}
---
+++ View/View.java
@@ -0,0 +1,32 @@
+package App;
+
+
+/**
+ * Created by lukasz on 06.05.2017.
+ */
+public class View {
+
+    public static void setView( char[][] array, int size) {
+        Go go = new Go();
+        go.HEIGHT = size;
+        go.WIDTH = size;
+        go.array = array;
+        new Thread() {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(Go.class);
+            }
+        }.start();
+    }
+    public static void main(String[] args) {
+        char[][] tab = new char[19][19];
+        tab[0][12] = 1;
+        tab[0][0] = 1;
+        tab[12][0] = 1;
+        tab[12][12] = 2;
+        tab[3][1] = 2;
+        setView(tab,13);
+    }
+
+
+}
