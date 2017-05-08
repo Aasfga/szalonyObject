315d4a4af04527c4037224af8f0ce27c7c512bbf
Dominik Grybos <dominik.grybos@gmail.com>
2017-05-08 14:57:16 +0200

    Move created

--- src/GoGame/View/Move.java
+++ src/GoGame/View/Move.java
@@ -11,31 +11,46 @@ import static GoGame.View.View.array;
 /**
  * Created by lukasz on 08.05.2017.
  */
-public class Move implements Input {
-    static String content = null;
-    static boolean getMove = false;
+public class Move implements Input
+{
+	static String content = null;
+	static boolean getMove = false;
 
-    public String getMove() {
-        getMove = true;
+	public String getMove()
+	{
+		getMove = true;
 
-        while(content == null)
-            try {
-                TimeUnit.MILLISECONDS.sleep(100);
-            } catch (InterruptedException e) {}
+		while(content == null)
+			try
+			{
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch(InterruptedException e)
+			{
+			}
 
-            String ans = content;
-        content = null;
-        return ans;
-    }
-    static void setMove(int x, int y, boolean color) {
-        if(!getMove)
-            return;
-        int index = 0;
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                array[y][x] = view.charA
-                index++;
-            }
-        }
-    }
+		String ans = content;
+		content = null;
+		return ans;
+	}
+
+	static void setMove(int x, int y, int color)
+	{
+		if(!getMove)
+			return;
+
+		getMove = false;
+		StringBuilder builder = new StringBuilder();
+
+		for(int i = 0; i < Go.HEIGHT; i++)
+		{
+			for(int j = 0; j < Go.WIDTH; j++)
+
+				if(x == j && i == y)
+					builder.append(color);
+				else
+					builder.append(View.array[i][j]);
+		}
+
+		content = builder.toString();
+	}
 }
--- src/TurnBasedGameLogic/Client/Input.java
+++ src/TurnBasedGameLogic/Client/Input.java
@@ -5,3 +5,26 @@ public interface Input
 {
 	String getMove();
 }
+
+
+/*
+	giveMe = true;
+	//content = null;
+
+
+	while(content == null)
+	{
+		TimeUnit.Milis.sleep(100);
+	}
+
+
+	ans = content;
+	content = null;
+
+	return ans;
+ */
+
+/*
+
+
+ */
